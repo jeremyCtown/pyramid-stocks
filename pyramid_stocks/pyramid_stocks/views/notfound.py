ce86ab0b01d68d20cd9511885ad8b6435b82ff90
Jeremy Crawford <jfcraw08@gmail.com>
2018-04-07 17:45:05 -0700

    added more doc strings

--- pyramid_stocks/pyramid_stocks/views/notfound.py
+++ pyramid_stocks/pyramid_stocks/views/notfound.py
@@ -3,5 +3,8 @@ from pyramid.view import notfound_view_config
 
 @notfound_view_config(renderer='../templates/404.jinja2')
 def notfound_view(request):
+    """
+    Returns 404 response if no route
+    """
     request.response.status = 404
     return {}
